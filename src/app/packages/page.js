1e98d7efff2d1933d078740ba61023bdd6d71355
Egokiphovwen Okiemute <Ego.okiemute@gmail.com>
2023-12-20 13:59:37 +0100

    Update page.js

--- src/app/packages/page.js
+++ src/app/packages/page.js
@@ -11,26 +11,37 @@ export default function Vision() {
             <div className="w-full">
                 <BannerComponent title="Our Services" sub="HOME / SERVICES" />
                 <div className="px-6 flex flex-col md:flex-row items-center md:justify-between gap-10 justify-center my-8 md:my-40 md:mt-[240px]">
-                    <div className="flex flex-col text-center items-center gap-5">
-                        <h1 className="text-2xl font-bold tracking-wide hidden md:block">EXPLORE US FROM THE EXPERIENCE</h1>
-                        <img src="/about-us.png" alt="About us" className="" />
-                    </div>
-                    <div className="flex flex-col space-y-5 w-2/3 text-base px-6">
-                        <h1 className="text-2xl font-bold tracking-wide md:hidden">EXPLORE US FROM THE EXPERIENCE</h1>
-                        <p>SuperIndexTradeFx is a premier investment firm that specializes in providing expert investment management services to individuals and institutions. Our team of seasoned investment professionals has decades of experience in the financial industry and is dedicated to helping clients achieve their financial goals.</p>
-                        <p>At SuperIndexTradeFx, we understand that the investment landscape can be complex and ever-changing, which is why we offer a diverse range of investment products and services to meet the unique needs of our clients. Whether you are a seasoned investor or just starting out, we have the tools and expertise to help you succeed.</p>
-                        <p>
-                            Our investment philosophy is centered around three core principles: discipline, risk management, and transparency. We believe that disciplined investment strategies are key to long-term success, and we work closely with our clients to develop customized investment plans that align with their goals and risk tolerance.
-                        </p>
-                        <p>
-                            At SuperIndexTradeFx, we believe that transparency is key to building trust with our clients. That's why we provide regular performance reports and open communication channels to keep our clients informed and engaged in the investment process.
-                        </p>
-                        <p>
-                            Our investment products and services include a range of equity and fixed-income strategies, as well as alternative investments such as real estate, private equity, and hedge funds. We also offer comprehensive financial planning services to help clients achieve their long-term financial goals.
-                        </p>
-                        <p>
-                            At SuperIndexTradeFx, we are committed to delivering exceptional investment management services that help our clients achieve their financial objectives. Contact us today to learn more about how we can help you succeed in the world of investing.
-                        </p>
+                    <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-15 px-4 md:px-8">
+                       <div className="flex flex-cols gap-10 items-start">
+                          <img src="" alt="bus" />
+                          <h1 className="font-semibold text-lg text-white uppercase">COURIER</h1>
+                          <p className="text-white">For our same-day service, shipments received before 10am are guaranteed to be delivered to selected airport cities</p>
+                       </div>
+                       <div className="flex flex-cols gap-10 items-start">
+                          <img src="" alt="car" />
+                          <h1 className="font-semibold text-lg text-white uppercase">LOGISTICS</h1>
+                          <p className="text-white">Our Domestic Economy Service has been specifically designed to suit both individual and corporate organisations</p>
+                       </div>
+                       <div className="flex flex-cols gap-10 items-start">
+                          <img src="" alt="cart" />
+                          <h1 className="font-semibold text-lg text-white uppercase">E-COMMERCE</h1>
+                          <p className="text-white">Our e-Commerce services can save you time and money in an increasingly competitive and fast changing business landscape.</p>
+                       </div>
+                       <div className="flex flex-cols gap-10 items-start">
+                          <img src="" alt="boat" />
+                          <h1 className="font-semibold text-lg text-white uppercase">INTERNATIONAL</h1>
+                          <p className="text-white">Our international express service delivers time sensitive documents and parcels nationwide to over 200 countries.</p>
+                       </div>
+                       <div className="flex flex-cols gap-10 items-start">
+                          <img src="" alt="hand" />
+                          <h1 className="font-semibold text-lg text-white uppercase">SPECIALISED SOLUTIONS</h1>
+                          <p className="text-white">Our customised business solutions can either address your entire supply chain or smaller elements within it such</p>
+                       </div>
+                       <div className="flex flex-cols gap-10 items-start">
+                          <img src="" alt="recycle" />
+                          <h1 className="font-semibold text-lg text-white uppercase">CUSTOM SOLUTIONS</h1>
+                          <p className="text-white">Every business has different supply chain challenges; which means that standard solutions are rarely effective.</p>
+                       </div>
                     </div>
                 </div>
                 <Footer />
