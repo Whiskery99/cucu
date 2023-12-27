6fd34e5b7f7c2a0479f8cd01a59fe9580ab41fb2
Matthewokiemute <ego.okiemute@gmail.com>
2023-12-27 17:37:02 +0100

    hello logos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ export default function Footer() {
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <a href="/" className="flex items-center">
-                            <img src="./logo.png" className="h-8 w-auto mr-1" alt=" Logo" />
+                            <img src="./logos.png" className="h-8 w-auto mr-1" alt=" Logo" />
                             <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Fratcht Logistics</span>
                         </a>
                     </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -108,7 +108,7 @@ export default function Navbar() {
             <nav className="relative bg-[#111] fixed w-full z-20 top-0 left-0 border-b border-gray-200">
                 <div className="w-screen flex flex-wrap items-center justify-between mx-auto p-4">
                     <div onClick={() => router.push('/')} className="flex items-center cursor-pointer mr-2">
-                        <img src="./logo.png" className="h-8 w-auto mr-1" alt=" Logo" />
+                        <img src="./logos.png" className="h-8 w-auto mr-1" alt=" Logo" />
                         <span className="self-center text-lg md:text-xl font-semibold text-white">FrachtLogistics</span>
                     </div>
 
